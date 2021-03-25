36af9c9af78816a4ec7f07f112c19049a44eb032
elena-cav <elenacavallero@outlook.com>
2021-03-25 21:49:35 +0000

    separate css

--- src/App.vue
+++ src/App.vue
@@ -15,5 +15,6 @@ export default {
 </script>
 
 <style scoped>
-
+@import url('https://fonts.googleapis.com/css2?family=Baloo+Tammudu+2:wght@400;600&display=swap&family=Montserrat:ital,wght@0,400;0,600;1,400;1,600&display=swap');
+@import './assets/styles.css';
 </style>
